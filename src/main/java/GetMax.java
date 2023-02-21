088080b6db370fb61f44b886a30ab4008a0e5681
vtran91 <vtran16@bellsouth.net>
2023-02-21 03:31:22 +0000

    Completed GetMax coding challenge

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,11 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int m = arr[0];
+        for (int i = 0; i < arr.length; i++)
+        {
+            m = m > arr[i]? m : arr[i];
+        }
+        return m;
     }
 }
